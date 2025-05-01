4123724a15c62a5073b5632b03e58b21499ccd14
ElizaSwanson <Liz99246@gmail.com>
2025-05-01 22:41:40 +0300

    latest11

--- users/models.py
+++ users/models.py
@@ -54,7 +54,6 @@ class Users(AbstractUser):
         related_name="custom_user_permissions_set",
         blank=True,
     )
-    is_active = True
 
     class Meta:
         verbose_name = "Пользователь"
